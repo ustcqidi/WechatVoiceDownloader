75de6676d44290fa67f8f0e754a45da1debcbdf0
Diven.Qi <diven.qi@zoom.us>
2021-01-26 21:39:44 +0800

    support download list from file

--- WeChatVoiceDownloader.go
+++ WeChatVoiceDownloader.go
@@ -4,20 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"bufio"
+	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func main() {
+func download(url string, file_idx int) {
+	fmt.Print("\nurl is ", url)
+	filename := strconv.Itoa(file_idx) + ".mp3"
+	fmt.Print("\nfilename is ", filename)
 
-	args := os.Args
-
-	url := args[1]
-	filename := args[2]
-
-	fmt.Print("\nurl ", url)
-	fmt.Print("\nname ", filename)
-
+	// Parse
 	response,_:=http.Get(url)
 	defer response.Body.Close()
 	body,_:=ioutil.ReadAll(response.Body)
@@ -29,7 +28,7 @@ func main() {
 
 	fmt.Print("download_link is ", download_link)
 
-	// Get the data
+	// Download
 	fmt.Print("\ndownload file...")
 	resp, err := http.Get(download_link)
 	if err != nil {
@@ -47,4 +46,44 @@ func main() {
 	fmt.Print("\nwrite file...")
 
 	ioutil.WriteFile(filename, data, 0644)
+}
+
+func main() {
+
+   filepath := "download.txt"
+   file, err := os.OpenFile(filepath, os.O_RDWR, 0666)
+   if err != nil {
+      fmt.Println("Open file error!", err)
+      return
+   }
+   defer file.Close()
+
+   fileIdx := 1
+
+   buf := bufio.NewReader(file)
+   for {
+      line, err := buf.ReadString('\n')
+	  line = strings.TrimSpace(line)
+	  download(line, fileIdx)
+	  fileIdx += 1
+    //   fmt.Println(line)
+      if err != nil {
+         if err == io.EOF {
+            fmt.Println("\nFinish!")
+            break
+         } else {
+            fmt.Println("Read file error!", err)
+            return
+         }
+      }
+   }
+
+	// args := os.Args
+
+	// url := args[1]
+	// filename := args[2]
+
+	// fmt.Print("\nurl ", url)
+	// fmt.Print("\nname ", filename)
+
 }
